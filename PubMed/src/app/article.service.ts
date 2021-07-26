5b25624808460bdecdc229483ef7c972906992f7
Nishtha Kalra <nishtha.kalra25@gmail.com>
2021-07-26 03:36:26 +0100

    complete search engine

--- PubMed/src/app/article.service.ts
+++ PubMed/src/app/article.service.ts
@@ -9,13 +9,16 @@ import {Article} from "./article";
   providedIn: 'root'
 })
 export class ArticleService {
-
+  baseURL: string = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/";
   constructor(private http: HttpClient) { }
 
-  getArticleIds(term: string): Observable<string[]> {
-    const url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&retmax=20&retmode=json&term=" + term;
-    return this.http.get<[]>(url)
-    //const ids = of(["1", "2"])
-    //return ids
+  getArticleIds(term: string): Observable<Object> {
+    const url = this.baseURL + "esearch.fcgi?db=pubmed&retmax=20&retmode=json&term=" + term;
+    return this.http.get(url);
+  }
+
+  getSummary(idParam: string): Observable<Object> {
+    const url = this.baseURL + "esummary.fcgi?db=pubmed&retmode=json&" + idParam;
+    return this.http.get(url);
   }
 }
--- PubMed/src/app/search/search.component.ts
+++ PubMed/src/app/search/search.component.ts
@@ -3,21 +3,111 @@ import { Article } from "../article";
 import { ArticleService } from "../article.service";
 
 @Component({
-  selector: 'app-search',
-  templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+    selector: 'app-search',
+    templateUrl: './search.component.html',
+    styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
+    articleIds: any = []
+    articleCount: number = 0;
+    articles: Article[] = [];
+    constructor(private articleService: ArticleService) { }
 
-  constructor(private articleService: ArticleService) { }
-  articleIds: string[] = [];
+    search(term: string): void {
+        this.articles = [];
+        this.articleCount = 0;
+        //console.log(term);
+        this.articleService.getArticleIds(term)
+        .subscribe(result => {
+            //console.log(result);
+            const ids = this.trimIds(result);
+            //console.log(ids);
+            this.articleIds = ids;
+            //console.log(this.articleIds);
+        });
+    }
 
-  search(term: string): void {
-    console.log(term);
-    this.articleService.getArticleIds(term).subscribe(ids => this.articleIds = ids)
-    console.log(this.articleIds);
-  }
-  ngOnInit(): void {
-  }
+    display() {
+        //console.log(this.articleIds);
+        this.articles = [];
+        this.articleCount = 0;
+        let idParam = "id=";
+        for (let id of this.articleIds) {
+            //console.log(id);
+            idParam = idParam + id + ",";
+        }
+        //console.log(idParam);
+        this.articleService.getSummary(idParam)
+            .subscribe(result => {
+                //console.log(result);
+                this.showArticles(result);
+            })
+    }
+
+    ngOnInit(): void {}
+
+    trimIds(result: Object): any {
+        let ids;
+        //console.log("inside trim ids");
+        Object.entries(result).forEach(entry => {
+            const [key, value] = entry;
+            if (key === "esearchresult") {
+                Object.entries(value).forEach(v => {
+                    const [k1, v1] = v;
+                    if (k1 === "idlist") {
+                        //console.log(v1);
+                        ids = v1;
+                    }
+                });
+            }
+        });
+        return ids;
+    }
+
+    showArticles(result: Object) {
+        console.log("inside show articles");
+        Object.entries(result).forEach(entry => {
+            const [k1, v1] = entry;
+            if (k1 === "result") {
+                Object.entries(v1).forEach(v => {
+                    const [k2, v2] = v;
+                    if (k2 === "uids") {
+                        // @ts-ignore
+                        this.articleCount = v2.length;
+                    } else {
+                        console.log(v2);
+                        let article: Article = {id: 0, author: [], link: "", journal: "", title: "", date: ""};
+                        // @ts-ignore
+                        article.id = v2.uid;
+                        // @ts-ignore
+                        article.date = v2.pubdate;
+                        // @ts-ignore
+                        article.title = v2.title;
+                        // @ts-ignore
+                        article.journal = v2.fulljournalname;
+                        // @ts-ignore
+                        article.link = "https://pubmed.ncbi.nlm.nih.gov/" + k2;
+                        // @ts-ignore
+                        console.log(v2.authors);
+                        // @ts-ignore
+                        console.log(typeof(v2.authors));
+                        // @ts-ignore
+                        Object.entries(v2.authors).forEach(a => {
+                            const [k3, v3] = a;
+                            console.log(v3);
+                            // @ts-ignore
+                            console.log(v3.name);
+                            // @ts-ignore
+                            article.author.push(v3.name);
+                        });
+                        // @ts-ignore
+                        //console.log(article);
+                        this.articles.push(article);
+                    }
+                });
+            }
+        });
+        console.log(this.articles);
+    }
 
 }
